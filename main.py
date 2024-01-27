46ff4db0953f1da103bb0519de502535add0ac94
Skonge080 <108742702+Skonge080@users.noreply.github.com>
2024-01-27 14:35:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import discord
 from discord.ext import commands, tasks
 import datetime
-import asyncio
 from keep_alive import keep_alive
 
 keep_alive()
@@ -15,19 +14,12 @@ log = True
 target_time1 = datetime.time(4, 0, 0)
 target_time2 = datetime.time(4, 0, 59)
 
-activity = discord.Activity(type=discord.ActivityType.watching,
-                            name="новый Мем")
+activity = discord.Activity(type=discord.ActivityType.watching, name="новый Мем")
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=PREFIX, intents=intents, activity=activity)
 
-
-async def send_log(message):
-  owner = await bot.fetch_user(OWNER_ID)
-  if owner:
-    await asyncio.sleep(1)
-    await owner.send(message)
-
+message_buffer = []
 
 async def upload_image():
   try:
@@ -38,34 +30,43 @@ async def upload_image():
       with open(file_path, 'rb') as file:
         await channel.send(file=discord.File(file))
     if log:
-      await send_log('image sent')
+      message_buffer.append('image sent')
       print('image sent')
     else:
       print('image sent')
   except Exception as e:
-    print(f'ERROR: {e}')
+    if log:
+      message_buffer.append(f'ERROR: {e}')
+      print(f'ERROR: {e}')
+    else:
+      print(f'ERROR: {e}')
 
+@tasks.loop(seconds=60)
+async def send_log():
+    if message_buffer:
+        owner = await bot.fetch_user(OWNER_ID)
+        if owner:
+            await owner.send("\n".join(message_buffer))
+            message_buffer.clear()
 
 @tasks.loop(seconds=60)
 async def daily_image():
   if log:
-    await send_log('Checking the time')
+    message_buffer.append('Checking the time')
     print('Checking the time')
   else:
     print('Checking the time')
   current_time = datetime.datetime.now().time()
   if target_time1 <= current_time <= target_time2:
     if log:
-      await send_log(
-          f'Right time; datetime: {datetime.datetime.now()}; {PREFIX}')
+      message_buffer.append(f'Right time; datetime: {datetime.datetime.now()}; {PREFIX}; {target_time1}')
       print(f'Right time; datetime: {datetime.datetime.now()}')
     else:
       print(f'Right time; datetime: {datetime.datetime.now()}')
     await upload_image()
   else:
     if log:
-      await send_log(
-          f'Wrong time; datetime: {datetime.datetime.now()}; {PREFIX}')
+      message_buffer.append(f'Wrong time; datetime: {datetime.datetime.now()}; {PREFIX}; {target_time1}')
       print(f'Wrong time; datetime: {datetime.datetime.now()}')
     else:
       print(f'Wrong time; datetime: {datetime.datetime.now()}')
@@ -75,7 +76,7 @@ async def daily_image():
 async def ping(ctx):
   await ctx.send('pong')
   if log:
-    await send_log('pong sent')
+    message_buffer.append('pong sent')
     print('pong sent')
   else:
     print('pong sent')
@@ -90,7 +91,7 @@ async def image(ctx):
 async def stop(ctx):
   daily_image.stop()
   if log:
-    await send_log('daily upload stopped')
+    message_buffer.append('daily upload stopped')
     print('daily upload stopped')
   else:
     print('daily upload stopped')
@@ -100,7 +101,7 @@ async def stop(ctx):
 async def start(ctx):
   daily_image.start()
   if log:
-    await send_log('daily upload started')
+    message_buffer.append('daily upload started')
     print('daily upload started')
   else:
     print('daily upload started')
@@ -109,11 +110,12 @@ async def start(ctx):
 @bot.event
 async def on_ready():
   if log:
-    await send_log(f'Logged in as {bot.user.name}')
+    message_buffer.append(f'Logged in as {bot.user.name}')
     print(f'Logged in as {bot.user.name}')
   else:
     print(f'Logged in as {bot.user.name}')
   daily_image.start()
+  send_log.start()
 
 
 bot.run(TOKEN)
