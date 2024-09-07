601288ef20158530fb7327c5d3a466198666b551
Skonge080 <108742702+Skonge080@users.noreply.github.com>
2024-09-07 18:33:14 +0500

    Added logging using the built-in module logging

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from discord.ext import commands, tasks
 import datetime
 import asyncio
 import random
+import logging
 from keep_alive import keep_alive, send_request
+
+
+logging.basicConfig(level=logging.INFO)
+discord.utils.setup_logging(level=logging.DEBUG)
+
 
 keep_alive()
 send_request()
@@ -37,50 +43,48 @@ async def upload_meme():
             with open(file_path, 'rb') as file:
                 await asyncio.sleep(random.uniform(0.1, 3.0))
                 await channel.send(file=discord.File(file))
-                print('meme sent')
+                logging.warning('Meme sent')
         else:
-            print('No file found with the specified number.')
+            logging.error('No file found with the specified number.')
     except Exception as e:
-        print(f'ERROR: {e}')
+        logging.error(f'ERROR: {e}')
 
 
 @tasks.loop(seconds=60)
 async def daily_meme():
-    print('Checking the time')
+    logging.info('Checking the time')
     current_time = datetime.datetime.now().time().replace(microsecond=0)
     if target_time1 <= current_time <= target_time2:
-        print(f'Right time; datetime: {datetime.datetime.now().replace(microsecond=0)}')
+        logging.warning(f'Right time; datetime: {datetime.datetime.now().replace(microsecond=0)}')
         await upload_meme()
     else:
-        print(f'Wrong time; datetime: {datetime.datetime.now().replace(microsecond=0)}')
+        logging.info(f'Wrong time; datetime: {datetime.datetime.now().replace(microsecond=0)}')
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    print('pong sent')
+    logging.info('Pong sent')
 
 @bot.command()
 async def meme(ctx):
     await upload_meme()
 
 @bot.command()
-async def stop(ctx):
-    daily_meme.stop()
-    print('daily upload stopped')
-    await ctx.send('daily upload stopped')
-
-@bot.command()
 async def start(ctx):
-    daily_meme.start()
-    print('daily upload started')
-    await ctx.send('daily upload started')
+    if daily_meme.is_running():
+        logging.warning('Daily upload is already running')
+        await ctx.send('Daily upload is already running')
+    else:
+        daily_meme.start()
+        logging.warning('Daily upload started')
+        await ctx.send('Daily upload started')
 
 @bot.command()
 async def pid(ctx):
     await asyncio.sleep(random.uniform(0.1, 3.0))
     await ctx.send(f'pid: {os.getpid()}, working time: {datetime.datetime.now().replace(microsecond=0) - start_time}')
-    print('pid sent')
+    logging.info('Pid sent')
 
 @bot.command()
 async def close(ctx, *, pid: str):
@@ -91,7 +95,7 @@ async def close(ctx, *, pid: str):
         await ctx.send("Пожалуйста, введите число.")
         return
     if pid == os.getpid():
-        print(f'{pid} closed')
+        logging.warning(f'{pid} closed')
         await ctx.send(f'{pid} closed')
         await asyncio.sleep(1)
         await bot.close()
@@ -101,7 +105,7 @@ async def close(ctx, *, pid: str):
 async def on_ready():
     global channel
     channel = bot.get_channel(CHANNEL_ID)
-    print(f'Logged in as {bot.user.name}')
+    logging.warning(f'Logged in as {bot.user.name}')
     daily_meme.start()
 
 
